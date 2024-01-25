11584db1ba9db85e37dc25bd5dcf9ed2733f5a15
王竞奇 <wangjingqi0829@gmail.com>
2024-01-25 06:22:42 +0000

    update contrib/PersonCount/Plugin1/CountPersonPostProcessor.h.
    Signed-off-by: 王竞奇 <wangjingqi0829@gmail.com>

--- contrib/PersonCount/Plugin1/CountPersonPostProcessor.h
+++ contrib/PersonCount/Plugin1/CountPersonPostProcessor.h
@@ -27,7 +27,7 @@ public:
 
     CountPersonPostProcessor(const CountPersonPostProcessor & other) = default;
 
-    APP_ERROR Init(const std::map<std::string, std::shared_ptr<void>> & postConfig) override;
+    APP_ERROR Init(const std::map<std::string, std::string> & postConfig) override;
 
     APP_ERROR DeInit() override;
     bool IsValidTensors(const std::vector<TensorBase> & tensors) const;

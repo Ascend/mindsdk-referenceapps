b8da22b261286a5543c2c6d935c28129305cb609
hid54630209 <kexiao3@huawei.com>
2024-08-15 07:01:51 +0000

    update contrib/FireDetection/python/main.py.
    Signed-off-by: hid54630209 <kexiao3@huawei.com>

--- contrib/FireDetection/python/main.py
+++ contrib/FireDetection/python/main.py
@@ -1,4 +1,6 @@
 import threading
+import signal
+import time
 import av
 from mindx.sdk import base
 from mindx.sdk.base import VideoDecoder, VideoDecodeConfig,\
@@ -7,7 +9,20 @@ from frame_analyzer import FrameAnalyzer
 from utils import infer_config, logger
 
 decoded_data_queue = []
+analyzed_data_queue = []
 decode_finished_flag = False
+SIGNAL_RECEIVED = False
+
+
+class Frame:
+    def __init__(self, image, frame_id):
+        self.image = image
+        self.frame_id = frame_id
+
+
+def stop_handler(signum, frame):
+    global SIGNAL_RECEIVED
+    SIGNAL_RECEIVED = True
 
 
 def vdec_callback_func(decoded_image, channel_id, frame_id):
@@ -20,17 +35,24 @@ def vdec_callback_func(decoded_image, channel_id, frame_id):
 
 def vdec_thread_func(vdec_config, vdec_callbacker, device_id, rtsp):
     global decode_finished_flag
+    global SIGNAL_RECEIVED
     with av.open(rtsp) as container:
         count = 0
         # 初始化VideoDecoder
         video_decoder = VideoDecoder(vdec_config, vdec_callbacker, device_id, 0)
         # 循环取帧解码
         for packet in container.demux():
+            if SIGNAL_RECEIVED:
+                break
+            if packet.size == 0:
+                logger.info("Finish to pull rtsp stream.")
+                SIGNAL_RECEIVED = True
+                break
             logger.debug("send packet:{} ".format(count))
             video_decoder.decode(packet, count)
+            time.sleep(0.02)
             count += 1
         logger.info("There are {} frames in total.".format(count))
-        decode_finished_flag = True
 
 
 # 视频编码回调函数
@@ -41,23 +63,36 @@ def venc_callback_func(output, output_datasize, channel_id, frame_id):
         file.write(output)
 
 
-def venc_thread_func(venc_config, venc_callbacker, device_id, model_path):
+def venc_thread_func(venc_config, venc_callbacker, device_id):
     video_encoder = VideoEncoder(venc_config, venc_callbacker, device_id)
-    frame_analyzer = FrameAnalyzer(model_path, device_id)
     i = 0
-    global decode_finished_flag
-    while not decode_finished_flag or not decoded_data_queue:
+    global SIGNAL_RECEIVED
+    while not (SIGNAL_RECEIVED and not decoded_data_queue):
         if not decoded_data_queue:
             continue
         frame_image = decoded_data_queue.pop(0)
         if i % infer_config["skip_frame_number"] == 0:
-            results = frame_analyzer.analyze(frame_image)
-            if results.size != 0:
-                frame_analyzer.alarm(results, i)
+            analyzed_data_queue.append(Frame(frame_image, i))
         video_encoder.encode(frame_image, i)
+        time.sleep(0.02)
         i += 1
+    logger.info("Venc thread ended.")
 
 
+def analyze_thread_func(model_path, device_id):
+    frame_analyzer = FrameAnalyzer(model_path, device_id)
+    global SIGNAL_RECEIVED
+    while not (SIGNAL_RECEIVED and not analyzed_data_queue):
+        if not analyzed_data_queue:
+            continue
+        frame = analyzed_data_queue.pop(0)
+        results = frame_analyzer.analyze(frame.image)
+        if results.size != 0:
+            frame_analyzer.alarm(results, frame.frame_id)
+    logger.info("Analyze thread ended.")
+
+
+signal.signal(signal.SIGINT, stop_handler)
 if __name__ == '__main__':
     base.mx_init()
     vdec_callbacker_instance = VdecCallBacker()
@@ -82,16 +117,23 @@ if __name__ == '__main__':
     vdec = threading.Thread(target=vdec_thread_func, kwargs={'vdec_config': vdec_conf,
                                                              'vdec_callbacker': vdec_callbacker_instance,
                                                              "device_id": infer_config["device_id"],
-                                                             "rtsp": infer_config["rtsp_url"]})
+                                                             "rtsp": infer_config["video_path"]})
 
     venc = threading.Thread(target=venc_thread_func, kwargs={'venc_config': venc_conf,
                                                              'venc_callbacker': venc_callbacker_instance,
-                                                             "device_id": infer_config["device_id"],
-                                                             "model_path": infer_config["model_path"]})
+                                                             "device_id": infer_config["device_id"]})
+
+    analyze = threading.Thread(target=analyze_thread_func, kwargs={"model_path": infer_config["model_path"],
+                                                                   "device_id": infer_config["device_id"]})
+
     # 启动线程
     vdec.start()
     venc.start()
+    analyze.start()
 
     # 等待执行完毕
     vdec.join()
     venc.join()
+    analyze.join()
+
+    logger.info("Fire detection task ended successfully.")

a969b5d824d19fe1169131f8a53702a37e06442d
吴宏阁 <wuhongge1@huawei.com>
2024-09-25 20:50:41 +0800

    prompt拼装

--- mxRAG/Samples/prompt_compressor/client.py
+++ mxRAG/Samples/prompt_compressor/client.py
@@ -8,6 +8,25 @@ import requests
 import html2text
 
 
+LOG_PROMPT_FORMAT = f'''
+你是一名高级硬件测试工程师，下面会给你一份测试日志和一些历史问题。
+测试日志中包含了该失败用例的相关日志信息，历史问题里每条记录由问题类型和问题描述组成。
+请按照如下要求进行日志分析：
+1、从测试日志中提取出断言失败之前的异常命令行；
+2、提取导致上一条命令行失败的关键信息，关键信息往往是命令行之前的回显信息；
+3、历史问题的记录由问题类型和问题描述组成，如果历史问题列表里存在与前面提取出来的关键信息相似的问题描述，请找出最相似的问题描述并且提取该条记录的问题类型；
+4、如果历史问题列表里没有相似问题描述，则根据分析从环境问题、脚本问题、版本缺陷中选出一个问题类型，不要给出其他答案；
+按json格式返回结果，其中包含以下json key：
+    分析过程，问题类型
+日志内容：
+```{context}```
+历史问题列表：
+```{history_label}```
+'''
+
+GENERAL_PROMPT_FORMAT = f"请基于给定的文章回答下述问题。\n\n##文章：{context}\n\n##请基于上述文章回答下面的问题。\n\n{question}\n##回答："
+
+
 def run_unstructured_doc_qa(server_url, file_path, question, target_tokens, target_rate):
     with open(file_path, 'r', encoding='utf-8') as f:
         context = f.read()
@@ -118,12 +137,18 @@ if __name__ == '__main__':
     if args.scenes == 'doc_qa_unstructured':
         compressed_text = run_unstructured_doc_qa(url, args.file_path, args.question, args.target_tokens,
                                                   args.target_rate)
+        prompt = GENERAL_PROMPT_FORMAT.format(context=compressed_text, question=args.question)
     elif args.scenes == 'doc_qa_structured':
         compressed_text = run_structured_doc_qa(url, args.file_path, args.question, args.topk)
+        prompt = GENERAL_PROMPT_FORMAT.format(context=compressed_text, question=args.question)
     elif args.scenes == 'log_analyse':
         compressed_text, history_label = run_log_analysis(url, args.file_path, args.question)
+        prompt = LOG_PROMPT_FORMAT.format(context=compressed_text, history_label=history_label)
     elif args.scenes == 'doc_summary':
         compressed_text = run_summary(url, args.file_path, args.question, args.compress_rate,
                                       args.embedding_batch_size, args.min_cluster_size)
+        prompt = GENERAL_PROMPT_FORMAT.format(context=compressed_text, question=args.question)
     else:
         raise ValueError('Unrecognized scenes')
+
+    print(f"文本压缩后的prompt为：{prompt}")

c8249b08180ff0d22e7a6991bd657b13cfb8110b
吴宏阁 <wuhongge1@huawei.com>
2024-09-25 20:26:05 +0800

    变量定义

--- mxRAG/Samples/prompt_compressor/server.py
+++ mxRAG/Samples/prompt_compressor/server.py
@@ -38,38 +38,41 @@ class SummaryItem(BaseModel):
 
 
 DOC_QA_MODEL_PATH = '/home/models/bge-reranker-v2-m3'
-DOC_QA_DEVICE_ID = 1
-
 DOC_SUMMARY_MODEL_PATH = '/home/models/bge-small-zh-v1.5'
 DOC_SUMMARY_DEVICE = 'npu:1'
+DEVICE_ID = 1
+X_DIM = 1024
+FAISS_INDEX_TYPE = 'FLAT:L2'
+FAISS_LOCAL_INDEX = './faiss.index'
+FAISS_EMBEDDING_MODEL = '/home/models/bge-large-en-v1.5'
+LOG_ANALYSE_LABEL_PATH = './data/log/history_label.jsonl'
 
-doc_qa = DocQaCompressor(DOC_QA_MODEL_PATH, DOC_QA_DEVICE_ID)
+doc_qa = DocQaCompressor(DOC_QA_MODEL_PATH, DEVICE_ID)
 log_analyse = LogAnalyseCompressor()
 doc_summary = ClusterSummary(DOC_SUMMARY_MODEL_PATH, DOC_SUMMARY_DEVICE)
 
-faiss = MindFAISS(x_dim=1024,
-                  index_type="FLAT:L2",
-                  devs=[1],
-                  load_local_index='./faiss.index',
-                  auto_save=False
-                  )
-embed = TextEmbedding('/home/models/bge-large-en-v1.5', dev_id=1)
-
-labels_path = './data/log/history_label.jsonl'
+faiss = MindFAISS(x_dim=X_DIM,
+                  index_type=FAISS_INDEX_TYPE,
+                  devs=[DEVICE_ID],
+                  load_local_index=FAISS_LOCAL_INDEX,
+                  auto_save=False)
+embed = TextEmbedding(FAISS_EMBEDDING_MODEL, dev_id=DEVICE_ID)
 label_list = []
-with open(labels_path, 'r') as f:
+with open(LOG_ANALYSE_LABEL_PATH, 'r') as f:
     for line in f:
         label_list.append(line)
 faiss.add(embed.embed_texts(label_list), [i for i in range(len(label_list))])
 
-def query_data_by_chunk(faiss, embed, reserved_list, label_list, topk: int = 5):
+
+def query_data_by_chunk(local_faiss, local_embed, reserved_list, local_label_list, topk: int = 5):
     if not isinstance(reserved_list, List):
         raise ValueError('The reserved_list must be of type List')
 
-    topk = min(topk, len(label_list))
+    topk = min(topk, len(local_label_list))
     q_len = len(reserved_list)
+    epsilon = 1e-10
 
-    ret = faiss.search(embed.embed_texts(reserved_list), k=topk)
+    ret = local_faiss.search(local_embed.embed_texts(reserved_list), k=topk)
     distances_list = ret[0]
     id_list = ret[1]
 
@@ -84,11 +87,11 @@ def query_data_by_chunk(faiss, embed, reserved_list, label_list, topk: int = 5):
     for distance in sorted_distances:
         for j in range(q_len):
             for i in range(topk):
-                if distances_list[j][i] == distance:
+                if abs(distances_list[j][i] - distance) < epsilon:
                     doc_id = int(id_list[j][i])
                     if doc_id not in tem_set:
                         tem_set.add(doc_id)
-                        text = label_list[doc_id]
+                        text = local_label_list[doc_id]
                         result_list.append((doc_id, text))
                     break
     return result_list[:topk] if len(result_list) > topk else result_list

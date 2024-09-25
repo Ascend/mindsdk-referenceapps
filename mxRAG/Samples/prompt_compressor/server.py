450e7b7c278052281778ee69346fba785319cdab
吴宏阁 <wuhongge1@huawei.com>
2024-09-25 16:00:38 +0800

    log场景添加few-shot

--- mxRAG/Samples/prompt_compressor/server.py
+++ mxRAG/Samples/prompt_compressor/server.py
@@ -7,6 +7,8 @@ from fastapi import FastAPI
 from mx_rag.summary import ClusterSummary
 from doc_qa_compressor import DocQaCompressor
 from log_analyse_compressor import LogAnalyseCompressor
+from mx_rag.storage.vectorstore import MindFAISS
+from mx_rag.embedding.local.text_embedding import TextEmbedding
 
 
 class DocQaUnstructuredItem(BaseModel):
@@ -45,6 +47,53 @@ doc_qa = DocQaCompressor(DOC_QA_MODEL_PATH, DOC_QA_DEVICE_ID)
 log_analyse = LogAnalyseCompressor()
 doc_summary = ClusterSummary(DOC_SUMMARY_MODEL_PATH, DOC_SUMMARY_DEVICE)
 
+faiss = MindFAISS(x_dim=1024,
+                  index_type="FLAT:L2",
+                  devs=[1],
+                  load_local_index='./faiss.index',
+                  auto_save=False
+                  )
+embed = TextEmbedding('/home/models/bge-large-en-v1.5')
+
+labels_path = './data/log/history_label.jsonl'
+label_list = []
+with open(labels_path, 'r', encoding='utf-8') as f:
+    for line in f:
+        label_list.append(line)
+faiss.add(embed.embed_texts(label_list), [i for i in range(len(label_list))])
+
+def query_data_by_chunk(faiss, embed, reserved_list, label_list, topk: int = 5):
+    if not isinstance(reserved_list, List):
+        raise ValueError('The reserved_list must be of type List')
+
+    topk = min(topk, len(label_list))
+    q_len = len(reserved_list)
+
+    ret = faiss.search(embed.embed_texts(reserved_list), k=topk)
+    distances_list = ret[0]
+    id_list = ret[1]
+
+    distances = [
+        distances_list[j][i]
+        for j in range(q_len)
+        for i in range(topk)
+    ]
+    sorted_distances = sorted(distances)[:q_len]
+    result_list = []
+    tem_set = set()
+    for distance in sorted_distances:
+        for j in range(q_len):
+            for i in range(topk):
+                if distances_list[j][i] == distance:
+                    doc_id = int(id_list[j][i])
+                    if doc_id not in tem_set:
+                        tem_set.add(doc_id)
+                        text = label_list[doc_id]
+                        result_list.append((doc_id, text))
+                    break
+    return result_list[:topk] if len(result_list) > topk else result_list
+
+
 app = FastAPI()
 
 
@@ -72,7 +121,11 @@ async def create_log_analyse_item(item: LogAnalyseItem):
     context = item.context
     question = item.question
 
-    return log_analyse.run_log_analysis(context, question)
+    compressed_context, reserved_list = log_analyse.run_log_analysis(context, question)
+    result_list = query_data_by_chunk(faiss, embed, reserved_list, label_list, 5)
+    label_data = '\n'.join([data for _, data in result_list])
+
+    return compressed_context, label_data
 
 
 @app.post("/doc_summary_compressor/")

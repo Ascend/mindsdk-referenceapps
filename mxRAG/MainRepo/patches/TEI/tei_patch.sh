e9472a94ea7e9229b0c32753f55a4973ec0c7426
root <root@localhost.localdomain>
2024-10-23 08:51:16 +0000

    patch folder change

--- mxRAG/MainRepo/patches/TEI/tei_patch.sh
+++ mxRAG/MainRepo/patches/TEI/tei_patch.sh
@@ -3,11 +3,6 @@ set -e
 
 PATCH_DIR=$(cd $(dirname $0); pwd)
 
-# 1. TEI打patch
+# TEI打patch
 cd $1
-patch -p1 < $PATCH_DIR/tei/tei.patch
-
-# 2. transformers打patch
-TRANSFORMER_PACKAGE_PATH=$(python3 -c 'import transformers; import os; print(os.path.dirname(transformers.__file__))')
-patch -p1 $TRANSFORMER_PACKAGE_PATH/models/bert/modeling_bert.py < $PATCH_DIR/ops/transformers_bert.patch
-patch -p1 $TRANSFORMER_PACKAGE_PATH/models/xlm_roberta/modeling_xlm_roberta.py < $PATCH_DIR/ops/transformers_xlm_roberta.patch
+patch -p1 < $PATCH_DIR/tei.patch
--- mxRAG/MainRepo/patches/optimize/xlm_roberta/tei/tei_patch.sh
+++ mxRAG/MainRepo/patches/optimize/xlm_roberta/tei/tei_patch.sh
@@ -5,4 +5,4 @@ PATCH_DIR=$(cd $(dirname $0); pwd)
 
 # transformers打patch
 TRANSFORMER_PACKAGE_PATH=$(python3 -c 'import transformers; import os; print(os.path.dirname(transformers.__file__))')
-patch -p1 $TRANSFORMER_PACKAGE_PATH/models/bert/modeling_bert.py < $PATCH_DIR/transformers_bert.patch
+patch -p1 $TRANSFORMER_PACKAGE_PATH/models/xlm_roberta/modeling_xlm_roberta.py < $PATCH_DIR/transformers_xlm_roberta.patch

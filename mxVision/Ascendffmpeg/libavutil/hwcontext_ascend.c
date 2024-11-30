bc5312c0f41829fae45b9b1372817347306b1d88
anan505 <1204828624@qq.com>
2024-11-30 07:17:07 +0000

    update mxVision/Ascendffmpeg/libavutil/hwcontext_ascend.c.
    Signed-off-by: anan505 <1204828624@qq.com>

--- mxVision/Ascendffmpeg/libavutil/hwcontext_ascend.c
+++ mxVision/Ascendffmpeg/libavutil/hwcontext_ascend.c
@@ -285,9 +285,6 @@ static int ascend_transfer_data_to(AVHWFramesContext *ctx, AVFrame *dst, const A
 
 static int ascend_transfer_data_from(AVHWFramesContext *ctx, AVFrame *dst, const AVFrame *src)
 {
-    AVHWDeviceContext *device_ctx = ctx->device_ctx;
-    AVASCENDDeviceContext *hwctx = ctx->hwctx;
-    AscendContext *ascend_ctx = hwctx->ascend_ctx;
 
     int i;
     size_t dstBytes;

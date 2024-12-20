e5a50cc8041afbb2fff9e0b1d489681546bb6eda
hid54630209 <kexiao3@huawei.com>
2024-12-20 09:01:28 +0000

    修改编译脚本
    Signed-off-by: hid54630209 <kexiao3@huawei.com>

--- tutorials/VideoEncoder&VideoDecoder/C++/build.sh
+++ tutorials/VideoEncoder&VideoDecoder/C++/build.sh
@@ -1,6 +1,13 @@
-# 删除build文件夹如果存在
-rm -rf build
-# 创建build文件夹并进入
+#!/bin/bash
+# 定义要检查的目录名
+dir_name="build"
+
+# 检查目录是否存在
+if [ -d "$dir_name" ]; then
+  # 删除目录及其内容
+  rm -rf "$dir_name"
+fi
+
 mkdir build
 cd build
 # 执行编译
